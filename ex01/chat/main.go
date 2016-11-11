b34d88a4a150eaca54d1de048cb3859da3bc09a0
Shota Yamamota <1230124fw@gmail.com>
2016-11-12 02:25:52 +0900

    pre chat

--- ex01/chat/main.go
+++ ex01/chat/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"html/template"
 	"log"
 	"net/http"
@@ -22,8 +23,14 @@ func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
+	var addr = flag.String("addr", ":8080", "The addr of the application.")
+	flag.Parse()
+	r := newRoom()
 	http.Handle("/", &templateHandler{filename: "chat.html"})
+	http.Handle("/room", r)
+	go r.run()
 	//web サーバーを開始します
+	log.Println("Starting web server on", *addr)
 	if err := http.ListenAndServe(":8080", nil); err != nil {
 		log.Fatal("ListenAndServer:", err)
 	}
